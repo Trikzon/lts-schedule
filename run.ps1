107ff5161575d60a9cc72763078c27afa2987ecf
Trikzon <trikzon@protonmail.com>
2021-07-14 14:41:31 -0400

    Add pitch-dark

--- run.ps1
+++ run.ps1
@@ -1,5 +1,6 @@
 node bin/lts.js -n " " -d "../projects/armor-visibility.json" -s 2020-06-01 -e 2022-06-01 -m -h out/armor-visibility.html -g out/armor-visibility.svg -p out/armor-visibility.png
 node bin/lts.js -n " " -d "../projects/flora-doubling.json" -s 2020-01-01 -e 2022-06-01 -m -h out/flora-doubling.html -g out/flora-doubling.svg -p out/flora-doubling.png
+node bin/lts.js -n " " -d "../projects/pitch-dark.json" -s 2021-06-01 -e 2022-06-01 -m -h out/pitch-dark.html -g out/pitch-dark.svg -p out/pitch-dark.png
 node bin/lts.js -n " " -d "../projects/shuffle.json" -s 2020-02-01 -e 2022-06-01 -m -h out/shuffle.html -g out/shuffle.svg -p out/shuffle.png
 node bin/lts.js -n " " -d "../projects/sneak-through-berries.json" -s 2020-01-01 -e 2022-06-01 -m -h out/sneak-through-berries.html -g out/sneak-through-berries.svg -p out/sneak-through-berries.png
 node bin/lts.js -n " " -d "../projects/transparent.json" -s 2020-04-01 -e 2022-06-01 -m -h out/transparent.html -g out/transparent.svg -p out/transparent.png
